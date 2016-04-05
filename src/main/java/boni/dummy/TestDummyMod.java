c307306685897d013480c1d39ae0d4c4dd9e442c
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-04-05 20:16:50 +0200

    Fix item model

--- src/main/java/boni/dummy/TestDummyMod.java
+++ src/main/java/boni/dummy/TestDummyMod.java
@@ -20,7 +20,7 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 public class TestDummyMod {
 
   public static final String MODID = "testdummy";
-  public static final String VERSION = "2.0";
+  public static final String VERSION = "1.10";
 
   @Mod.Instance
   public static TestDummyMod instance;
